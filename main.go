74e6e09d150d243a76050029ebc14b6cfe395662
Roberto Teixeira Méndez <rtm0x17@users.noreply.github.com>
2023-09-17 20:13:19 +0200

    POST request with template block

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Film struct {
@@ -14,9 +15,40 @@ type Film struct {
 func main() {
 
 	http.HandleFunc("/", httpHandlerIndex)
+	http.HandleFunc("/add-film", httpHandlerAddFilm)
+
 	// Catch error and logs it and exit the application
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
+
+func httpHandlerAddFilm(resposeWriter http.ResponseWriter, request *http.Request) {
+	log.Print("HTMX request")
+	log.Print(request.Header.Get("HX-Request"))
+
+	title := request.PostFormValue("title")
+	director := request.PostFormValue("director")
+	time.Sleep(1 * time.Second)
+	tpl := template.Must(template.ParseFiles("templates/index.html"))
+	tpl.ExecuteTemplate(resposeWriter, "film-list-item", Film{Title: title, Director: director})
+}
+
+//func httpHandlerAddFilm(resposeWriter http.ResponseWriter, request *http.Request) {
+//	log.Print("HTMX request")
+//	log.Print(request.Header.Get("HX-Request"))
+//
+//	title := request.PostFormValue("title")
+//	director := request.PostFormValue("director")
+//
+//	htmlStr := fmt.Sprintf("<p>%s - %s</p>", title, director)
+//
+//	tmpl, _ := template.New("t").Parse(htmlStr)
+//	time.Sleep(1 * time.Second)
+//	err := tmpl.Execute(resposeWriter, nil)
+//
+//	if err != nil {
+//		return
+//	}
+//}
 
 func httpHandlerIndex(resposeWriter http.ResponseWriter, request *http.Request) {
 	films := map[string][]Film{
